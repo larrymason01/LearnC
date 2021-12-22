88ce2f4a4bf1f0494ae5eb3bfdd0af60f563d7ef
Larry Mason <lbmason110108@gmail.com>
2021-12-22 17:56:17 -0500

    Created WhileLoops.c

---
+++ WhileLoops.c
@@ -0,0 +1,31 @@
+// While and Do-While Loops
+#include "stdio.h"
+
+int main () {
+
+    // While loops are pre-test loops
+    // Counter controlled while loop
+    int count = 1;
+    while (count <= 5) {
+        printf("%i\n", count);
+        count++;
+    }
+
+    // Logic controlled While Loop
+    int num;
+    scanf("%d", &num);
+    while (num != -1) {
+        scanf("%d", &num);
+    }
+
+    // Do-while Loops are post-test loops.  It will always run at least once
+    int i;
+    do {
+        scanf("%d", &i);
+    } while (i != 20);
+
+    // While loops are usually used for logic controlled situations, while for loops are for counter controlled.
+
+    return 0;
+}
+
