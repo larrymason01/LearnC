abf15e094cd0193ec4a816449717b6e1d148c458
Larry Mason <lbmason110108@gmail.com>
2021-12-22 19:14:07 -0500

    Created Arrays.c

---
+++ Arrays.c
@@ -0,0 +1,29 @@
+// Creating and using Arrays
+#include "stdio.h"
+
+int main () {
+
+    long numbers[10];   // The number inside the array is the size of the array
+
+    numbers[0] = 1; // The first element in the array has index 0
+
+    printf("%ld\n", numbers[0]);  // Prints the first array element
+
+    // Simple program to calculate grade average
+    int grades[10];
+    int sum = 0;
+    float average;
+
+    for (int i = 0; i < 10; i++) {
+        printf("Enter grade %d: ", i + 1);
+        scanf("%d", &grades[i]);
+        sum += grades[i];
+    }
+
+    average = sum / 10.0f;
+
+    printf("Sum of grades: %d\n", sum);
+    printf("Grade average: %0.2f", average);
+
+    return 0;
+}
