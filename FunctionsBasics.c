80834709fcb60f2ad0c2798d27f85002b8eb2744
Larry Mason <lbmason110108@gmail.com>
2021-12-23 17:30:58 -0500

    Created FunctionBasics.c

---
+++ FunctionsBasics.c
@@ -0,0 +1,20 @@
+// Basics of Functions
+#include "stdbool.h"
+#include "stdio.h"
+
+// Function Prototype
+void printHello();
+
+
+int main(int argc, char *argv[]) {
+    // Calling the function
+    printHello();
+
+
+    return 0;
+}
+
+// Defining the function
+void printHello() {
+    printf("Hello");
+}
