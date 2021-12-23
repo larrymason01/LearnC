1b0b467544b684b8bf50ec7ea842ed49959dc059
Larry Mason <lbmason110108@gmail.com>
2021-12-22 22:05:32 -0500

    Created MultidimensionalArrays.c

---
+++ MultidimensionalArrays.c
@@ -0,0 +1,22 @@
+// Multidimensional Arrays
+#include "stdio.h"
+
+int main() {
+    // 2D Arrays
+
+    int matrix[4][5] = {    // Example of a 2D array
+        {10, 20, 30, 40, 50},
+        {60, 70, 80, 90, 100},
+        {110, 120, 130, 140, 150},
+        {160, 170, 180, 190, 200}
+    };
+
+    int num = matrix[1][2]; // This will set num to 80  (2nd row[1], column 3[2])
+    printf("%d", num);
+
+    // Think of a 2D array as a table with rows and columns
+
+    int table[4][3] = {[0][0] = 2}; // Initializing a specific element in the 2D array
+
+    return 0;
+}
