56b983a03e4d0fd19987cea3a2d8bf41aa657fbe
Larry Mason <lbmason110108@gmail.com>
2021-12-27 12:00:12 -0500

    Created DefiningPointers.c

---
+++ DefiningPointers.c
@@ -0,0 +1,19 @@
+/*
+// Defining Pointers
+
+#include "stdio.h"
+#include "stddef.h"
+
+int main(int argc, char *argv[]) {
+
+    // The value of a pointer is a memory address
+    int number = 85;
+    int *pnumber = &number; // Sets the pointer pnumber to the memory address of number.  & is the address of operator
+    printf("number is stored at address: %p\n", pnumber);  // %p is the format specifier for pointers
+
+    int *pWord = NULL;  // NULL is the equivilant of 0 for pointers. The pointer does not point to anything specific.
+    printf("pWord points to: %p", pWord);
+
+    return 0;
+}
+*/
